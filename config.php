1d3ea51b90a36917500cbcfa6614ffcc96d932b4
liammceleney23-23 <liam@zopollo.co.uk>
2025-12-28 00:10:48 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Configuration file for IT Support PWA
+ * IMPORTANT: Keep this file secure and never commit to public repositories
+ */
+
+return [
+    'vapid' => [
+        'subject' => 'mailto:liam@zopollo.co.uk',
+        'publicKey' => 'BPgIfLcgqH3VWy1ICrxuYV-o4EGwlcloVfOKlt9ZWHxq4qjU69pc-mDx28AyouFpGyTHe87YCPzapTD5Yfkj14I',
+        'privateKey' => 'al7IFwBNRVn3rX8xk_AMf3VP0F4rAlXYxeb5I_VtNYY'
+    ]
+];
+?>
